a593600baf4dbd99257678b9d2d66e7d3df81c22
Jonata Weber <jonataweber@gmail.com>
2015-08-07 22:47:55 -0300

    Update 2015-08-08T01:47:52.616Z

--- elements/bootstrap/sw-toolbox-setup.js
+++ elements/bootstrap/sw-toolbox-setup.js
@@ -8,7 +8,7 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-var swToolboxURL = new URL('../sw-toolbox/sw-toolbox.js', params.get('baseURI')).href;
+var swToolboxURL = new URL('elements/sw-toolbox/sw-toolbox.js', params.get('baseURI')).href;
 importScripts(swToolboxURL);
 
 if (params.has('defaultCacheStrategy')) {
